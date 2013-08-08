b1857679953ba5fb62fa2ac0228cf95c911af293
Benjamin Chelli <benjamin@chelli.net>
2013-08-08 01:46:24 -0400

    Simplify event managment

--- boilerplate/events.js
+++ boilerplate/events.js
@@ -4,16 +4,25 @@
 
   Torpedo.events = {};
 
-  var eventList = [];
-  var nativeEventsCalls = {};
-  var nativeEventsFn = {};
+  var eventCallbacks = {};
+  var eventStacks = {};
 
-  function nativeEventsFactory(eventName){
-    return function(){
-      for(var i=0,l=nativeEventsFn[eventName].length;i<l;i++){
-        nativeEventsFn[eventName][i]();
+  function attachNativeEvent(eventName){
+
+    // init event stack
+    eventStacks[eventName] = [];
+
+    // create the generic callback
+    eventCallbacks[eventName] = function(){
+      for(var i=0,l=eventStacks[eventName].length;i<l;i++){
+        eventStacks[eventName][i]();
       }
     }
+
+    // attach event
+    var ev = eventName.split('.');
+    $(window[ev[0]]).on(ev[1], eventCallbacks[eventName]);
+
   }
 
 
@@ -21,45 +30,34 @@
 
     el = $(el || '#torpedo-page');
 
-    // add to event list
-    eventList.push({
-      el:             el
-    , classSelector:  classSelector
-    , eventName:      eventName
-    , func:           func
-    });
-
     // attach native events
     if(eventName.indexOf('.')!==-1){
-      if(!nativeEventsFn[eventName]){
-        ;(function(eventName){
-          var ev = eventName.split('.');
-          nativeEventsFn[eventName] = []
-          nativeEventsCalls[eventName] = nativeEventsFactory(eventName);
-          $(window[ev[0]]).on(ev[1], nativeEventsCalls[eventName]);
-        })(eventName);
+      if(!eventCallbacks[eventName]){
+        attachNativeEvent(eventName);
       }
-      nativeEventsFn[eventName].push(func);
+      eventStacks[eventName].push(func);
     } else {
-      // attach delegate
-      el.delegate(classSelector, eventName, func);
+      // attach events to first nodes
+      el.filter(classSelector).on(eventName, func);
+      // attach events to descendents
+      el.find(classSelector).on(eventName, func);
     }
   }
 
   Torpedo.events.detachEvents = function(){
-    var ev;
-    while(ev = eventList.shift()){
+
+    for(var eventName in eventCallbacks){
 
       // detach native events
-      if(ev.eventName.indexOf('.')!==-1 && nativeEvents[ev.eventName]){
-        nativeEventsFn[ev.eventName].length = 0;
-        nativeEventsFn[ev.eventName] = false;
-        $(window[evs[0]]).off(evs[1], nativeEventsCalls[eventName]);
-      } else {
-        // detach delegate
-        ev.el.undelegate(ev.classSelector, ev.eventName, ev.func);
-      }
+      var ev = eventName.split('.');
+      $(window[ev[0]]).off(ev[0], eventCallbacks[eventName]);
+
+      // remove from event stack
+      eventStacks[eventName].length = 0;
+      eventCallbacks[eventName] = false;
+
     }
+
   }
 
 })();
